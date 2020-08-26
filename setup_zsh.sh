ebb02b57d6e7cd8e2d8c4459e8a41a059f86b19f
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2020-08-26 23:38:30 +0300

    Update setup_zsh.sh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,25 +1,27 @@
-cp .zshrc $HOME/
-cp .p10k.zsh $HOME/
-cp .taskrc $HOME/
+# Link dotfiles
+ln -s .zshrc $HOME/.zshrc
+ln -s .p10k.zsh $HOME/.p10k.zsh
+ln -s .taskrc $HOME/.taskrc
 
+# Install Oh my zsh
 export KEEP_ZSHRC=yes 
 export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install zsh navigation tools
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/psprint/zsh-navigation-tools/master/doc/install.sh)"
 
-
+# Install p10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git 
 cp -r powerlevel10k ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/
 cp -r powerlevel10k/* ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/
 rm -rf powerlevel10k
 
+# Install fonts
 git clone https://github.com/romkatv/powerlevel10k-media/
 mkdir -p ~/.local/share/fonts
 cp powerlevel10k-media/*.ttf ~/.local/share/fonts
 rm -rf powerlevel10k-media
 
-cp ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k/powerlevel10k.zsh-theme ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/
-
+# Set zsh default shell (requires password)
 chsh -s $(which zsh)
-
