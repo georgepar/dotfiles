29f8bbfaec1b91de735f8facda0d1e4b71381d99
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2020-08-28 19:01:46 +0300

    add neovim to installation

--- install_packages.sh
+++ install_packages.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y \
     git git-lfs curl wget zsh build-essential cmake \
-    imagemagick direnv unzip tmux taskwarrior vim htop
+    imagemagick direnv unzip tmux taskwarrior vim neovim htop
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 $HOME/.fzf/install --bin
