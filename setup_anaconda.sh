7074c41558967f5e93474cc551afa8ef7a15380a
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2021-10-18 14:56:05 +0300

    anaconda install basic tools

--- setup_anaconda.sh
+++ setup_anaconda.sh
@@ -3,7 +3,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh -O anac
 bash anaconda.sh -b -p $HOME/opt/anaconda3
 
 #$HOME/opt/anaconda3/bin/pip install -Ur requirements.txt
-
+$HOME/opt/anaconda3/bin/pip install -Ur pudb black bpython jupyter
 #ln -s flake8 $HOME/.config/flake8
 
 rm anaconda.sh
